c5af50d4c9d446242bf1e2475728ce556a08fbab
Jason Michels <michelsja@icloud.com>
2016-09-24 13:59:25 -0500

    Updates to the query to implement repositories and entity interface

--- query.go
+++ query.go
@@ -6,6 +6,9 @@ import (
 	"os"
 )
 
+/**
+Open connection to mysql
+ */
 func Open() (*sql.DB, error) {
 	database := os.Getenv("DB_DATABASE")
 	dbUser := os.Getenv("DB_USERNAME")
@@ -13,16 +16,61 @@ func Open() (*sql.DB, error) {
 	dbHost := os.Getenv("DB_HOST")
 	conn := dbUser + ":" + dbPassword + "@tcp(" + dbHost + ":3306)/" + database
 
-	return sql.Open("mysql", conn)
+	return sql.Open(os.Getenv("DB_CONNECTION"), conn)
 }
 
-type MySql struct {
+/**
+Interface for entities that will use a repository
+ */
+type Entity interface {
+	GenerateInsertSql() string
+	ToInsertArgs() []interface{}
+}
+
+/**
+Base repository struct
+ */
+type Repository struct {
+	LastInsertId int64
 	Db *sql.DB
 }
 
-func (mySql *MySql) Query(query string) ([]map[string]interface{}, error) {
+/**
+Find all query
+ */
+func (repo *Repository) FindAll(query string) ([]map[string]interface {}, error) {
+	return repo.Query(query)
+}
 
-	rows, err := mySql.Db.Query(query)
+/**
+Store a single repository into database
+ */
+func (repo *Repository) Store(entity Entity) error {
+	stmtIns, err := repo.Db.Prepare(entity.GenerateInsertSql())
+	if err != nil {
+		return err
+	}
+	defer stmtIns.Close()
+
+	res, err := stmtIns.Exec(entity.ToInsertArgs()...)
+	if err != nil {
+		return err
+	}
+
+	repo.LastInsertId, err = res.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+/**
+Make a query against the database
+ */
+func (repo *Repository) Query(query string) ([]map[string]interface{}, error) {
+
+	rows, err := repo.Db.Query(query)
 	if err != nil {
 		return nil, err
 	}
